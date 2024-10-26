6f7480ae98e7555304bd2b071312314c399fa8a3
Dominykas Černovas <cernovas50@gmail.com>
2024-10-26 18:37:14 +0300

    Setting CORS

---
+++ ReactApp1/ReactApp1.Server/Models/Entities/Order.cs
@@ -0,0 +1,52 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ReactApp1.Server.Models;
+
+[Table("Order")]
+public class Order
+{
+    public Order()
+    {
+        FullOrders = new HashSet<FullOrder>();
+        Payments = new HashSet<Payment>();
+    }
+    
+    [Key]
+    [Column("OrderId")]
+    public int OrderId { get; set; }
+
+    [Column("Status")]
+    public int Status { get; set; }
+
+    [Column("CreatedByEmployeeId")]
+    public int CreatedByEmployeeId { get; set; }
+    
+    [Column("ReceiveTime")]
+    public DateTime ReceiveTime { get; set; }
+
+    [Column("DiscountPercentage")]
+    public int? DiscountPercentage { get; set; }
+    
+    [Column("DiscountFixed")]
+    public decimal? DiscountFixed { get; set; }
+    
+    [Column("PaymentId")]
+    public int? PaymentId { get; set; }
+    
+    [Column("Refunded")]
+    public bool Refunded { get; set; }
+    
+    [Column("ReservationId")]
+    public int? ReservationId { get; set; }
+    
+    [ForeignKey(nameof(CreatedByEmployeeId))]
+    public virtual Employee CreatedByEmployee { get; set; }
+    
+    [ForeignKey(nameof(ReservationId))]
+    public virtual Reservation Reservation { get; set; }
+    
+    public virtual ICollection<FullOrder> FullOrders { get; set; }
+    
+    public virtual ICollection<Payment> Payments { get; set; }
+}
---
+++ ReactApp1/reactapp1.client/config.ts
@@ -0,0 +1,2 @@
+// BASE API URL
+export const API_BASE_URL = "http://localhost:5114";
