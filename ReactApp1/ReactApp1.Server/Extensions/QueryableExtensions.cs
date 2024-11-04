2816536da3870288028b18da220be810edd4288f
rytiskar <rytis.karalius321@gmail.com>
2024-11-04 21:34:46 +0200

    Implement item CRUD functionality using repository pattern

---
+++ ReactApp1/ReactApp1.Server/Extensions/QueryableExtensions.cs
@@ -0,0 +1,12 @@
+namespace ReactApp1.Server.Extensions
+{
+    public static class QueryableExtensions
+    {
+        public static IQueryable<T> Paginate<T>(this IQueryable<T> source, int pageNumber, int pageSize)
+        {
+            return source
+                .Skip((pageNumber - 1) * pageSize)
+                .Take(pageSize);
+        }
+    }
+}
