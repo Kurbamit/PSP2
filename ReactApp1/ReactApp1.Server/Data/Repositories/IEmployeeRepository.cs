30009684f6f0973c88bcc3edf7a652c662adc8a5
Dominykas Černovas <113939016+Kurbamit@users.noreply.github.com>
2024-12-02 08:19:10 +0200

    Employee FE (#15)
    * Updated gitignore
    
    * Employee FE

--- ReactApp1/ReactApp1.Server/Data/Repositories/IEmployeeRepository.cs
+++ ReactApp1/ReactApp1.Server/Data/Repositories/IEmployeeRepository.cs
@@ -8,7 +8,7 @@ namespace ReactApp1.Server.Data.Repositories
     {
         Task<PaginatedResult<Employee>> GetAllEmployeesAsync(int pageNumber, int pageSize);
         Task<EmployeeModel?> GetEmployeeByIdAsync(int employeeId);
-        Task AddEmployeeAsync(EmployeeModel employee, int? establishmentId);
+        Task<int> AddEmployeeAsync(EmployeeModel employee, int? establishmentId);
         Task UpdateEmployeeAsync(EmployeeModel employee);
         Task DeleteEmployeeAsync(int employeeId);
     }
--- ReactApp1/ReactApp1.Server/Services/IEmployeeService.cs
+++ ReactApp1/ReactApp1.Server/Services/IEmployeeService.cs
@@ -8,7 +8,7 @@ public interface IEmployeeService
 {
     Task<PaginatedResult<Employee>> GetAllEmployees(int pageSize, int pageNumber);
     Task<EmployeeModel?> GetEmployeeById(int employeeId);
-    Task CreateNewEmployee(EmployeeModel employee, int? establishmentId);
+    Task<int> CreateNewEmployee(EmployeeModel employee, int? establishmentId);
     Task UpdateEmployee(EmployeeModel employee);
     Task DeleteEmployee(int employeeId);
 }
