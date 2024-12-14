a78e5ce0faf32149d7db47891e8013c93f31d91c
Dominykas Černovas <cernovas50@gmail.com>
2024-12-14 12:49:14 +0200

    Clean index page

--- ReactApp1/reactapp1.client/src/App.tsx
+++ ReactApp1/reactapp1.client/src/App.tsx
@@ -7,11 +7,9 @@ import Register from "./components/Base/Register.tsx";
 import Login from "./components/Base/Login.tsx";
 import Logout from "./components/Base/Logout.tsx";
 import './App.css';
-import { API_BASE_URL } from "../config";
 import Cookies from 'js-cookie';
 import Employees from "./components/Domain/Employee/Employees.tsx";
 import EmployeeDetail from "./components/Domain/Employee/EmployeeDetail.tsx";
-import SelectDropdown from "./components/Base/SelectDropdown.tsx";
 import Orders from "./components/Domain/Order/Orders.tsx";
 import OrderDetail from "./components/Domain/Order/OrderDetail.tsx";
 import { ErrorProvider } from "./components/Base/ErrorContext.tsx";
@@ -21,29 +19,19 @@ import Services from './components/Domain/Service/Services.tsx';
 import ServiceDetail from './components/Domain/Service/ServiceDetail.tsx';
 import Receipt from "./components/Domain/Order/Receipt.tsx";
 
-interface Forecast {
-    date: string;
-    temperatureC: number;
-    temperatureF: number;
-    summary: string;
-}
-
 function App() {
-    const [forecasts, setForecasts] = useState<Forecast[]>();
     const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
 
     useEffect(() => {
         const token = Cookies.get('authToken');
         if (token) {
             setIsLoggedIn(true);
-            populateWeatherData();
         }
     }, []);
 
     const handleLogin = (token: string) => {
         Cookies.set('authToken', token, { expires: 1 });
         setIsLoggedIn(true);
-        populateWeatherData();
     };
 
     const handleLogout = () => {
@@ -51,65 +39,26 @@ function App() {
         setIsLoggedIn(false);
     };
 
-    const contents = forecasts === undefined
-        ? <div>
-            <p><em>Loading... Please refresh once the ASP.NET backend has started.</em></p>
-            <p>Error might be because You're not logged in.</p>
+    const IndexPage = () => (
+        <div>
+            <h1>Welcome to the POS System</h1>
+            {isLoggedIn ? (
+                <p>You are logged in. You can now access the contents.</p>
+            ) : (
+            <p>Login in order to see contents.</p>)}
         </div>
-        : <>
-            <div>
-                <h1>Example Page</h1>
-                <SelectDropdown
-                    endpoint="/AllItems"
-                    onSelect={(item) => console.log("Selected item:", item)}
-                />
-            </div>
-            <table className="table table-striped" aria-labelledby="tabelLabel">
-                <thead>
-                <tr>
-                    <th>Date</th>
-                    <th>Temp. (C)</th>
-                    <th>Temp. (F)</th>
-                    <th>Summary</th>
-                </tr>
-                </thead>
-                <tbody>
-                {forecasts.map(forecast =>
-                    <tr key={forecast.date}>
-                        <td>{forecast.date}</td>
-                        <td>{forecast.temperatureC}</td>
-                        <td>{forecast.temperatureF}</td>
-                        <td>{forecast.summary}</td>
-                    </tr>
-                )}
-                </tbody>
-            </table>
-        </>;
+    );
 
     return (
         <ErrorProvider>
-             {/* Ensure GlobalAlert is within ErrorProvider */}
             <InnerApp
-                contents={contents}
+                contents={IndexPage()}
                 isLoggedIn={isLoggedIn}
                 handleLogin={handleLogin}
                 handleLogout={handleLogout}
             />
         </ErrorProvider>
     );
-
-    async function populateWeatherData() {
-        const token = Cookies.get('authToken');
-        const response = await fetch(`${API_BASE_URL}/GetWeatherForecast`,
-            {
-                method: 'GET',
-                headers: {
-                    'Authorization': `Bearer ${token}`,
-                },
-            });
-        const data = await response.json();
-        setForecasts(data);
-    }
 }
 
 function InnerApp({
@@ -133,8 +82,6 @@ function InnerApp({
                 <Routes>
                     <Route path="/" element={
                         <div>
-                            <h1 id="tabelLabel">Weather forecast</h1>
-                            <p>This component demonstrates fetching data from the server.</p>
                             {contents}
                         </div>
                     } />
