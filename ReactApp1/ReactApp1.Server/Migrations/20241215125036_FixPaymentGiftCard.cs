ab7c58eda22d0726785e277bb649414da15fe17d
DzefersonS <108546929+DzefersonS@users.noreply.github.com>
2024-12-15 14:55:49 +0200

    Add payment bugfix (#50)

---
+++ ReactApp1/ReactApp1.Server/Migrations/20241215125036_FixPaymentGiftCard.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ReactApp1.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixPaymentGiftCard : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Payment_GiftCard_GiftCardId",
+                table: "Payment");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Payment_GiftCardId",
+                table: "Payment");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Payment_GiftCardId",
+                table: "Payment",
+                column: "GiftCardId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Payment_GiftCard_GiftCardId",
+                table: "Payment",
+                column: "GiftCardId",
+                principalTable: "GiftCard",
+                principalColumn: "GiftCardId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
